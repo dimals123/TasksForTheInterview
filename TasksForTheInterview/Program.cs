bdae0d3317bf88091fb1a7e94324bf03e79c0c07
Дмитрий <dimals97@hotmail.com>
2022-12-07 01:11:16 +0300

    Добавьте файлы проекта.

---
+++ TasksForTheInterview/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using TasksForTheInterview;
+
+const string COOKING_BREAKFAST = "CookingBreakfast";
+
+LambdaMeter(COOKING_BREAKFAST, () => Breakfast.StartCooking());
+
+void LambdaMeter(string label, Action act)
+{
+    var sw = new Stopwatch();
+    sw.Start();
+    act();
+    sw.Stop();
+    Console.WriteLine($"{label} : {sw.Elapsed}");
+}
